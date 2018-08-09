828b70d1df502acf57c0ac23f6035693c1c8ab93
pranguelov <pranguelov@aegistg.com>
2018-08-09 09:07:40 -0500

    AI controller finding player tank

--- BattleTank/Source/BattleTank/TankAIController.cpp
+++ BattleTank/Source/BattleTank/TankAIController.cpp
@@ -1,6 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankAIController.h"
+#include "TankPlayerController.h"
+#include "Engine/World.h"
+
 
 
 ATank * ATankAIController::GetControlledTank() const
@@ -8,8 +11,27 @@ ATank * ATankAIController::GetControlledTank() const
 	return Cast<ATank>(GetPawn());
 }
 
+ATank * ATankAIController::GetPlayerTank() const
+{
+	ATank* playerTank = nullptr;
+	APlayerController* playerController = GetWorld()->GetFirstPlayerController();
+	if (playerController)
+	{
+		ATankPlayerController* tankPlayerController = Cast<ATankPlayerController>(playerController);
+		if (tankPlayerController)
+		{
+			playerTank = tankPlayerController->GetControlledTank();
+		}
+
+	}
+
+	return playerTank;
+}
+
 void ATankAIController::BeginPlay()
 {
 	Super::BeginPlay();
-	UE_LOG(LogTemp, Warning, TEXT("ATankAIController::BeginPlay %s"), *(GetControlledTank()->GetName()));
+	UE_LOG(LogTemp, Warning, TEXT("ATankAIController::BeginPlay. Controlled Tank %s"), *(GetControlledTank()->GetName()));
+
+	UE_LOG(LogTemp, Warning, TEXT("ATankAIController::BeginPlay Player Tank %s"), *(GetPlayerTank()->GetName()));
 }
